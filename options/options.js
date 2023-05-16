37d0ad4bea10b77d7ca90f190fa849344951264a
Remy Mocquet <remy-mocquet@live.be>
2023-05-17 00:34:01 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "misty-feather-40741",
-  url: "https://misty-feather-40741.botics.co",
-  api: "https://misty-feather-40741.botics.co/api/v1"
-}
+  "name": "misty-feather-40741",
+  "url": "https://misty-feather-40741.botics.co",
+  "api": "https://misty-feather-40741.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
