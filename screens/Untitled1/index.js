7decb7d8c2a996ccb2b9c7201d9c991c9d564040
Remy Mocquet <remy-mocquet@live.be>
2023-05-17 00:36:23 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
@@ -8,13 +8,23 @@ const Untitled1 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><View style={styles.KkqrAZPJ}></View></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: '100%',
+    backgroundColor: "#FAF9FF"
+  },
+  KkqrAZPJ: {
+    height: 46,
+    width: 230,
+    backgroundColor: "#BBB2E9",
+    borderRadius: 50,
+    color: "#777777",
+    left: 129,
+    top: 350
   }
 });
 export default Untitled1;
